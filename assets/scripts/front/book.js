f71066c47a1475310581e69335f30aad201103e9
JC <jc@jc.com>
2019-05-01 00:20:38 +0200

    Fix book page (slider + videos)

--- assets/scripts/front/book.js
+++ assets/scripts/front/book.js
@@ -1,40 +1,58 @@
 import Glide from '@glidejs/glide'
 import { getOffsetElement } from '../component/position';
 
-const initBookTabs = () => {
-    initTabActions();
-    document.getElementById('book-slider') && initSlider();
+class BookTabs {
+    slider;
+
+    constructor() {
+        this.slider = new Glide('#book-slider');
+        this.initActions();
+        this.showSlider();
+    };
+
+    /**
+     * Allows to initialize actions.
+     */
+    initActions() {
+        document
+            .querySelector('#book-tabs [data-tab="tab-slider"]:not([disabled]')
+            .addEventListener('click', () => { this.showSlider(); })
+        ;
+        document
+            .querySelector('#book-tabs [data-tab="tab-videos"]:not([disabled]')
+            .addEventListener('click', () => { this.showVideos(); })
+        ;
+        document.getElementById('center-button').addEventListener('click', () => {
+            const offset = getOffsetElement(document.getElementById('book-slider'));
+            window.scroll({
+                top: offset.top,
+                left: offset.left,
+                behavior: 'smooth'
+            });
+        });
+    };
+
+    /**
+     * Display slider tab.
+     */
+    showSlider() {
+        document.getElementById('tab-videos').classList.remove('active');
+        document.getElementById('tab-slider').classList.add('active');
+        this.slider.mount();
+    };
+
+    /**
+     * Display videos tab.
+     */
+    showVideos() {
+        document.getElementById('tab-slider').classList.remove('active');
+        document.getElementById('tab-videos').classList.add('active');
+        this.slider.destroy();
+    };
 }
 
-const initTabActions = () => {
-    [...document.querySelectorAll('#book-tabs [data-tab]:not([disabled])')].forEach(
-        (element) => {
-            element.addEventListener('click', () => {switchTab(element.dataset.tab);});
-        }
-    );
-};
-
-const switchTab = (tabIdentifier) => {
-    console.log('switchTab', tabIdentifier);
-    [...document.querySelectorAll('.tab')].forEach(
-        (element) => {
-            element.classList.remove('active');
-        }
-    );
-    document.getElementById(tabIdentifier).classList.add('active');
-};
-
-const initSlider = () => {
-    new Glide('#book-slider').mount();
-
-    document.getElementById('center-button').addEventListener('click', () => {
-        const offset = getOffsetElement(document.getElementById('book-slider'));
-        window.scroll({
-            top: offset.top,
-            left: offset.left,
-            behavior: 'smooth'
-        });
-    });
+const initBookTabs = () => {
+    new BookTabs();
 };
 
 document.getElementById('book-tabs') && initBookTabs();
